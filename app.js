194755a53033c0489d7b411e64e2bb8b626026e5
DavidNavas898 <dnavascampus@gmail.com>
2026-06-12 16:31:31 -0500

    feat: :sparkles: Añadiendo funciones camposRepetidos eliminar editar limpiar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let usuarioDB = []
-
+const botonFormulario = document.getElementById("main-container-bottom-form")
 
 
 
@@ -12,6 +12,15 @@ function crearUsuario(){
     const telefono = document.getElementById("telefono").value;
     const cargo = document.getElementById("cargo").value;
     const password = document.getElementById("password").value;
+    const limpiar = document.getElementById("limpiar");
+
+    if(!validarFormulario()){
+        return;
+    }
+
+     if(!camposRepetidos()){
+        return;
+    }
 
     const usuario = {
         id,
@@ -47,16 +56,25 @@ function crearUsuario(){
     const editar = document.createElement("button")
     editar.textContent = "Editar"
     editar.classList.add("edit")
+    editar.addEventListener("click",() =>{
+        editarUsuario(tarjeta,usuario);
+    })
     
     const eliminar = document.createElement("button")
     eliminar.textContent = "Eliminar"
     eliminar.classList.add("delete")
+    eliminar.addEventListener("click", ()=>{
+        eliminarUsuario(tarjeta,id)
+    })
 
+   
  
     
     divBotones.append(editar,eliminar)
     divBotones.classList.add("botones")
     tarjeta.append(identificacion,nombre,correo,tel,cargoTitulo,divBotones);
+
+
     
     tarjeta.classList.add("tarjeta-usuario")
 
@@ -67,3 +85,94 @@ function crearUsuario(){
 
 }
 
+function validarFormulario(){
+    const campos = [
+        {input: document.getElementById("cedula"), error: document.getElementById("error-cedula"), mensaje: "*La cedula es obligatoria"},
+        {input: document.getElementById("name"), error: document.getElementById("error-name"), mensaje: "*El nombre es obligatorio"},
+        {input: document.getElementById("email"), error: document.getElementById("error-email"), mensaje: "*El email es obligatorio"},
+        {input: document.getElementById("telefono"), error: document.getElementById("error-telefono"), mensaje: "*El telefono es obligatorio"},
+        {input: document.getElementById("password"), error: document.getElementById("error-password"), mensaje: "*La contraseña es obligatoria"},
+    ]
+
+    let valido = true;
+
+    campos.forEach(p =>{
+        if(!p.input.value.trim()){
+            p.input.classList.add("inputError");
+            p.error.textContent = p.mensaje;
+            valido = false
+        }else{
+            p.input.classList.remove("inputError")
+            p.error.textContent = ""
+        }
+    })
+
+    return valido;
+
+
+
+}
+
+function camposRepetidos(){
+    const cedula = document.getElementById("cedula")
+    const errorCedula = document.getElementById("error-cedula")
+
+    const existe = usuarioDB.some(usuario => usuario.id === cedula.value);
+
+    if(existe){
+        cedula.classList.add("inputError");
+        errorCedula.textContent = "La cedula ya existe"
+        return false;
+    }
+
+    cedula.classList.remove("inputError")
+    errorCedula.textContent = "";
+    return true;
+}
+
+function eliminarUsuario(tarjeta,id){
+    tarjeta.remove()
+
+    usuarioDB = usuarioDB.filter(usuario => usuario.id != id);
+
+    document.querySelector(".users-top p").textContent = `${usuarioDB.length} registros`;
+}
+
+function editarUsuario(tarjeta,usuario){
+    document.getElementById("cedula").value = usuario.id;
+    document.getElementById("name").value = usuario.name; 
+    document.getElementById("email").value = usuario.email;
+    document.getElementById("telefono").value = usuario.telefono;
+    document.getElementById("password").value = usuario.password;
+
+    botonActualizar = document.createElement("div");
+    botonActualizar.classList.add("azul");
+    botonActualizar.textContent = "Actualizar";
+
+
+    botonCrear = document.getElementById("crear");
+    botonCrear.classList.add("esconder");
+
+    botonLimpiar = document.getElementById("limpiar");
+    botonLimpiar.setAttribute("onclick", "locationReload()");
+    botonLimpiar.textContent = "Cancelar Edicion"
+
+    botonFormulario.append(botonActualizar);
+
+    
+
+    
+    
+}
+
+function limpiarFormulario(usuario){
+    document.getElementById("cedula").value = "";
+    document.getElementById("name").value = ""; 
+    document.getElementById("email").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("password").value = "";
+}
+
+function locationReload(){
+    location.reload();
+}
