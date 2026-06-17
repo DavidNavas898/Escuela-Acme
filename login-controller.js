075fd1fc48f3e8484c84d53669e248e75aac4d9a
jrssj <jr8465207@gmail.com>
2026-06-17 15:13:40 -0500

    fix: arreglo de errores

--- login-controller.js
+++ login-controller.js
@@ -15,7 +15,7 @@ if (miFormulario) {
         let accesoConcedido = false;
         let rolUsuario = "";
 
-        for (let i = 0; i < listaUsuarios.length; i++) {
+        for (let i = 0; i < listaUsuarios.lenght; i++) {
             const usuarioActual = listaUsuarios[i];
             if (usuarioActual.email === correoIngresado && usuarioActual.password === claveIngresada) {
                 accesoConcedido = true;
@@ -34,7 +34,7 @@ if (miFormulario) {
                 confirmButtonColor: '#3085d6'
             }).then((result) => {
                 if (result.isConfirmed && rolUsuario === 'administrativo') {
-                    window.location.href = 'portaladmin.html';
+                    window.location.href = 'crear_examen.html';
                 }else{
                      window.location.href = 'inicio.html';
                 }
